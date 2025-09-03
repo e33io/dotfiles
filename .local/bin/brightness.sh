925b9b849f842a5696bcc37acf0e5396cd9f608b
e33io <info@e33.io>
2025-09-02 19:41:58 -0500

    Update bash shebang

--- .local/bin/brightness.sh
+++ .local/bin/brightness.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # =============================================================================
 # Change display brightness with brightnessctl and send dunst notification
