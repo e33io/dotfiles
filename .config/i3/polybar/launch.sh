658f1b016c5cbde141b244d542d9b7fa434898b3
e33io <info@e33.io>
2025-09-17 23:48:02 -0500

    Update Polybar config location

--- .config/i3/polybar/launch.sh
+++ .config/i3/polybar/launch.sh
@@ -3,8 +3,9 @@
 # Terminate already running bar instances
 killall -q polybar
 
-# Launch Polybar, using default config location ~/.config/polybar/config
+# Launch Polybar, using custom config location
 echo "---" | tee -a /tmp/polybar.log
-polybar i3bar 2>&1 | tee -a /tmp/polybar.log & disown
+polybar -c $HOME/.config/i3/polybar/config.ini i3bar 2>&1 \
+| tee -a /tmp/polybar.log & disown
 
 echo "Polybar launched..."
